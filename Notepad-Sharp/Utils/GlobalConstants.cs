6058d7650c7b22ba325768783325a925ce89b138
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2024-05-22 14:56:22 -0500

    Removed unnecessary class

--- Notepad-Sharp/Utils/GlobalConstants.cs
+++ Notepad-Sharp/Utils/GlobalConstants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NotepadSharp.Utils
+﻿namespace NotepadSharp.Utils
 {
     public class GlobalConstants
     {
