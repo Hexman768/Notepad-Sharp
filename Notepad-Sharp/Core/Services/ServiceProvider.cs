1120da7eedef1249034dcb32c214f43fed611250
Zachary Pedigo <Zach.Pedigo@Oracle.com>
2025-01-16 09:45:15 -0600

    Added more documentation

--- Notepad-Sharp/Core/Services/ServiceProvider.cs
+++ Notepad-Sharp/Core/Services/ServiceProvider.cs
@@ -4,15 +4,31 @@ using System.Collections.Generic;
 
 namespace NotepadSharp.Core.Services
 {
+    /// <summary>
+    /// This is the one-stop service provider class for the
+    /// entire application. This object will return an instance
+    /// of any service that is used in the project.
+    /// </summary>
     sealed class ServiceProvider : IServiceProvider
     {
         private static Dictionary<Type, object> _serviceDict = new Dictionary<Type, object>();
 
+        /// <summary>
+        /// Adds a service instance to the service dictionary.
+        /// </summary>
+        /// <param name="serviceType"></param>
+        /// <param name="instance"></param>
         public void AddService(Type serviceType, object instance)
         {
             _serviceDict.Add(serviceType, instance);
         }
 
+        /// <summary>
+        /// Returns an instance of a service that matches the given type.
+        /// </summary>
+        /// <param name="serviceType"></param>
+        /// <returns></returns>
+        /// <exception cref="ServiceNotFoundException"></exception>
         public object GetService(Type serviceType)
         {
             lock (_serviceDict)
--- Notepad-Sharp/Core/Services/ServiceSingleton.cs
+++ Notepad-Sharp/Core/Services/ServiceSingleton.cs
@@ -2,10 +2,17 @@
 
 namespace NotepadSharp.Core.Services
 {
+    /// <summary>
+    /// This is a Singleton class that returns an instance of a
+    /// service that matches the given object type.
+    /// </summary>
     public static class ServiceSingleton
     {
         volatile static IServiceProvider instance = new ServiceProvider();
 
+        /// <summary>
+        /// Instance of the <see cref="IServiceProvider"/>.
+        /// </summary>
         public static IServiceProvider ServiceProvider
         {
             get
@@ -22,6 +29,12 @@ namespace NotepadSharp.Core.Services
             }
         }
 
+        /// <summary>
+        /// Returns an instance of a service that matches
+        /// the given object type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static T GetRequiredService<T>()
         {
             var service = instance.GetService(typeof(T));
